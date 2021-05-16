de216c23193eebeec52d2a9cbfd5681758f72c01
Nouu <noelarrieulacoste@yahoo.fr>
2021-05-16 11:48:21 +0200

    fix some issues in code

--- src/database/factories/UserFactory.ts
+++ src/database/factories/UserFactory.ts
@@ -1,24 +1,24 @@
-import { User, Role } from '../../api/entities/User';
+import {Role, User} from '../../api/entities/User';
 import bcrypt from 'bcrypt';
 import * as faker from 'faker';
 
 export default async (data?: Partial<User>): Promise<User> => {
-  let password = await bcrypt.hash(faker.random.word(), 12);
-  if (data && data.password) {
-    password = await bcrypt.hash(data.password, 12);
-  }
-  const randomRole: Role = faker.random.arrayElement([
-    'user',
-    'staff',
-    'admin',
-  ]);
+    let password = await bcrypt.hash(faker.random.word(), 12);
+    if (data && data.password) {
+        password = await bcrypt.hash(data.password, 12);
+    }
+    const randomRole: Role = faker.random.arrayElement([
+        Role.ADMIN,
+        Role.CLIENT,
+        Role.STAFF
+    ]);
 
-  const user = User.create({
-    firstName: (data && data.firstName) || faker.name.firstName(),
-    lastName: (data && data.lastName) || faker.name.lastName(),
-    email: (data && data.email) || faker.internet.email(),
-    password,
-    role: (data && data.role) || randomRole,
-  });
-  return user;
+    const user = User.create({
+        firstName: (data && data.firstName) || faker.name.firstName(),
+        lastName: (data && data.lastName) || faker.name.lastName(),
+        email: (data && data.email) || faker.internet.email(),
+        password,
+        role: (data && data.role) || randomRole,
+    });
+    return user;
 };
