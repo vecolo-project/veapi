a99bc1bab58a5f44cd2e03639b4fdfd9c1858886
Clément BOSSARD <cbossard3@myges.fr>
2021-06-06 11:30:38 +0200

    bikeManufacturer route

---
+++ src/api/routes/bikeManufacturer.ts
@@ -0,0 +1,96 @@
+import { Router } from 'express';
+import { checkRole, isAuth } from '../middlewares';
+import { Role } from '../entities/User';
+import { celebrate, Joi } from 'celebrate';
+import { Container } from 'typedi';
+import BikeManufacturerService from '../services/BikeManufacturerService';
+
+const route = Router();
+const paramsRules = celebrate({
+  body: Joi.object({
+    name: Joi.string().required(),
+    phone: Joi.string().min(10).max(16).required(),
+    address: Joi.string().required(),
+  }),
+});
+const basePath = '/bikeManufacturer';
+const defaultService = BikeManufacturerService;
+
+route.post(
+  basePath,
+  isAuth,
+  checkRole(Role.STAFF),
+  paramsRules,
+  async (req, res, next) => {
+    const service = Container.get(defaultService);
+    try {
+      const entityResult = await service.create(req.body);
+      return res.status(201).json(entityResult);
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+route.get(basePath, isAuth, checkRole(Role.STAFF), async (req, res, next) => {
+  try {
+    const service = Container.get(defaultService);
+    const offset = req.body.offset || 0;
+    const limit = req.body.limit || 20;
+    const entityResult = await service.find({ offset, limit });
+    return res.status(200).json(entityResult);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+route.get(
+  basePath + ':id',
+  isAuth,
+  checkRole(Role.STAFF),
+  async (req, res, next) => {
+    try {
+      const service = Container.get(defaultService);
+      const id = Number.parseInt(req.params.id);
+      const entityResult = await service.findOne(id);
+      return res.status(200).json(entityResult);
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+route.delete(
+  basePath + ':id',
+  isAuth,
+  checkRole(Role.STAFF),
+  async (req, res, next) => {
+    try {
+      const service = Container.get(defaultService);
+      const id = Number.parseInt(req.params.id);
+      const articles = await service.delete(id);
+      return res.status(204).json(articles);
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+route.put(
+  basePath + ':id',
+  isAuth,
+  checkRole(Role.STAFF),
+  paramsRules,
+  async (req, res, next) => {
+    const service = Container.get(defaultService);
+    const id = Number.parseInt(req.params.id);
+    try {
+      const entityResult = await service.update(id, req.body);
+      return res.status(201).json(entityResult);
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+export default route;
