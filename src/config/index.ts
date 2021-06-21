a9b46a61f24f4a1de1ad8c65796087d7f3b78fcc
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-06-21 16:39:00 +0200

    fix loadenv issue

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 dotenv.config();
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 export interface Config {
   port: number | string;
