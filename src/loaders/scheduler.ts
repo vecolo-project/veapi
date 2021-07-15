876c8733318688f933d3369f9121747c05eb24d3
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-07-15 15:14:01 +0200

    batch purge ready (run at 3 am)

--- src/loaders/scheduler.ts
+++ src/loaders/scheduler.ts
@@ -1,11 +1,11 @@
 import * as schedule from 'node-schedule';
-import {Container} from 'typedi';
-import StationMonitoringService from '../api/services/StationMonitoringService';
+import { Container } from 'typedi';
+import SchedulerService from '../api/services/SchedulerService';
 
 export default (): void => {
-  schedule.scheduleJob('* * * * *', async () => {
-    console.log('CRON');
-    const service = Container.get(StationMonitoringService);
-    console.log(await service.getRepo().count());
+  schedule.scheduleJob('0 3 * * *', async () => {
+    console.log('Batch purge monitoring execution');
+    const service = Container.get(SchedulerService);
+    service.purgeMonitoring();
   });
 };
