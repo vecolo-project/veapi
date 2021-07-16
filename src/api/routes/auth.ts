17be0ee24a27e4f25d7175434a516bbae4c0e358
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-07-16 18:26:37 +0200

    reset password ready

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -3,6 +3,10 @@ import { Container } from 'typedi';
 import { celebrate, Joi } from 'celebrate';
 import UserService from '../services/UserService';
 import { Logger } from 'winston';
+import { attachUser, isAuth } from '../middlewares';
+import { userRequest } from '../../types/userRequest';
+import bcrypt from 'bcrypt';
+import EmailSenderService from '../services/EmailSenderService';
 
 const route = Router();
 
@@ -49,6 +53,69 @@ route.post(
         req.body.password
       );
       return res.json({ user, token }).status(200);
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+route.post(
+  '/reset-password',
+  celebrate({
+    body: Joi.object({
+      token: Joi.string().required(),
+      newPassword: Joi.string().min(4).required(),
+      confirmNewPassword: Joi.string()
+        .min(4)
+        .required()
+        .valid(Joi.ref('newPassword')),
+    }),
+  }),
+  async (req: userRequest, res, next) => {
+    const service = Container.get(UserService);
+    try {
+      const currentUser = await service.findOneByResetToken(req.body.token);
+      if (!currentUser) {
+        return res.status(403).json({
+          error: 'Token non valide',
+        });
+      }
+      currentUser.password = await bcrypt.hash(req.body.newPassword, 12);
+      currentUser.resetPasswordToken = '';
+      await service.update(currentUser.id, currentUser);
+      return res.status(200).end();
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+route.get(
+  '/forgot-password',
+  celebrate({
+    query: Joi.object({
+      email: Joi.string().required(),
+    }),
+  }),
+  async (req: userRequest, res, next) => {
+    const userService = Container.get(UserService);
+    const emailSenderService = Container.get(EmailSenderService);
+    try {
+      const currentUser = await userService.findOneByEmail(
+        req.query.email?.toString()
+      );
+      if (!currentUser) {
+        return res.status(403).json({
+          error: 'Email non valide',
+        });
+      }
+      const resetLink = await userService.generatePasswordToken(currentUser);
+      await emailSenderService.sendResetPassword(
+        currentUser.firstName,
+        currentUser.lastName,
+        currentUser.email,
+        resetLink
+      );
+      return res.status(200).end();
     } catch (e) {
       return next(e);
     }
