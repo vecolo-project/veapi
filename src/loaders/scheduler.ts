23c98978f8ecda25dd58b8d28e95282ebbe1c7fa
Nouu <noelarrieulacoste@yahoo.fr>
2021-07-17 12:48:10 +0200

    better visibility on when shcedules runs

--- src/loaders/scheduler.ts
+++ src/loaders/scheduler.ts
@@ -7,12 +7,14 @@ export default (): void => {
   schedule.scheduleJob('0 3 * * *', async () => {
     Logger.info('Batch purge monitoring execution');
     const service = Container.get(SchedulerService);
-    service.purgeMonitoring();
+    await service.purgeMonitoring();
+    Logger.info('Batch purge done');
   });
 
   schedule.scheduleJob('0 3 1 * *', async () => {
     Logger.info('Generate monthly subscriptions invoice');
     const service = Container.get(SchedulerService);
-    service.genereateMonthlyUsersInvoice();
+    await service.genereateMonthlyUsersInvoice();
+    Logger.info('Subscription invoice generated');
   });
 };
